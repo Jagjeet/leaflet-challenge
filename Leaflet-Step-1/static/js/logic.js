81d7535957559ad1dda85992668fb937dc111fef
Jagjeet Khalsa <Jagjeet.Khalsa@gmail.com>
2021-10-15 21:25:33 -0700

    Add map with some basic bootstrap styling for popup

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -16,6 +16,54 @@ when a marker is clicked.
 -Create a legend that will provide context for your map data.
 */
 
+function createMap(earthquakeLayer) {
+
+  console.log(earthquakeLayer);
+
+  // Create the tile layer that will be the background of our map.
+  // let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  //     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  // });
+
+  let streetmap =  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+  });
+
+
+  // Create a baseMaps object to hold the streetmap layer.
+  let baseMaps = {
+    "Street Map": streetmap
+  };
+
+  // Create an overlayMaps object to hold the earthquakes layer.
+  let overlayMaps = {
+    "Earthquakes": earthquakeLayer
+  };
+
+  // Create the map object with options.
+  // Set the center of the world to Vacaville, CA obviously
+  // 38.3566° N, 121.9877° W
+  // Okay ... the Atlantic Ocean works much better!
+  // 14.5994° S, 28.6731° W
+  let map = L.map("map-id", {
+    // center: [38.3566, 121.9877],
+    center: [14.5994, 28.6731],
+    zoom: 3,
+    layers: [streetmap, earthquakeLayer]
+  });
+
+  // Create a layer control, and pass it baseMaps and overlayMaps. Add the layer control to the map.
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(map);
+
+}
+
 function createMarkers(response) {
   console.log(response);
 
@@ -26,13 +74,19 @@ function createMarkers(response) {
     let dateString = new Date(e.properties.time).toDateString();
 
     return L.marker([e.geometry.coordinates[1], e.geometry.coordinates[0]])
-      .bindPopup(`<h2>Title: ${e.properties.title}<h2>
-                  <h3>ID: ${e.id}</h3>
-                  <h3>Time: ${dateString}</h3>
-                  <h3>Depth: ${e.geometry.coordinates[2]}</h3>`);
+      .bindPopup(`<div class="card>
+                    <div class="card-header"><h5>${e.properties.title}</h5></div>
+                    <div class="card-body">
+                      <div>ID: ${e.id}</div>
+                      <div>Magnitude: ${e.properties.mag}</div>
+                      <div>Time: ${dateString}</div>
+                      <div>Depth: ${e.geometry.coordinates[2]}km</div>
+                    </div>
+                  </div>`);
   });
 
-  console.log(earthquakeMarkers);
+  // console.log(earthquakeMarkers);
+  createMap(L.layerGroup(earthquakeMarkers));
 }
 
 // Perform an API call to the Earthquake USGS API to get the earthquake information. Call createMarkers when it completes.
