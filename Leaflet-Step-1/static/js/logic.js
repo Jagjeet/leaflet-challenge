c279fff851353bd9a8fe2a0790ae68e8a5594637
Jagjeet Khalsa <Jagjeet.Khalsa@gmail.com>
2021-10-15 18:17:43 -0700

    Create initial set of markers

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -7,7 +7,7 @@ by their size and and depth of the earth quake by color. Earthquakes
 with higher magnitudes should appear larger and earthquakes with greater
 depth should appear darker in color.
 
--HINT the depth of the earth can be found as the third coordinate for
+-HINT the depth of the earthquake can be found as the third coordinate for
 each earthquake.
 
 -Include popups that provide additional information about the earthquake
@@ -18,6 +18,21 @@ when a marker is clicked.
 
 function createMarkers(response) {
   console.log(response);
+
+  let earthquakes = response.features;
+
+  let earthquakeMarkers = earthquakes.map(function(e) {
+    // Using https://stackoverflow.com/questions/40761205/javascript-convert-timestamp-to-human-readable-date
+    let dateString = new Date(e.properties.time).toDateString();
+
+    return L.marker([e.geometry.coordinates[1], e.geometry.coordinates[0]])
+      .bindPopup(`<h2>Title: ${e.properties.title}<h2>
+                  <h3>ID: ${e.id}</h3>
+                  <h3>Time: ${dateString}</h3>
+                  <h3>Depth: ${e.geometry.coordinates[2]}</h3>`);
+  });
+
+  console.log(earthquakeMarkers);
 }
 
 // Perform an API call to the Earthquake USGS API to get the earthquake information. Call createMarkers when it completes.
